e5266d26f35fcee91a17cabb2a84575efdd8007b
Mushfiqur Rahman <Mushfiqur Rahman>
2017-09-16 15:32:13 +0600

    Ping Pong!

--- index.js
+++ index.js
@@ -4,16 +4,29 @@ const Bot = new Discord.Client();
 const BotToken = 'MzU4NTE1Mzk3MDgwOTA3ODAz.DJ54Jg.aPb0MKQOuNhVCOlWnxYAKb7fhNA'
 const prefix = '/>'
 
+const Commands = {
+	"Ping": function(parameters, msgObj){
+		msgObj.Channel.send("Pong!");
+	}
+}
+
 Bot.on('ready', () => {
 	console.log('Bot Initiated');
 });
+
+function ProcessCommand(content, msgObj) {
+	var words = content.split("");
+	if (Commands[words[0]]) {
+		Commands[words[0]](words.splice(0, 1), msgObj);
+	}
+}
 
 Bot.on('message', (Message) => {
 	if (Message.author == Bot) return;
 
 	var content = Message.content.toLowerCase
 	if (StringM(content.startsWith).startsWith(prefix)) {
-
+		ProcessCommand(StringM(content).chompLeft("/>"), Message);
 	}
 });
 Bot.on('guildMemberAdd', (NewMember) => {
