daca9a9bbad79baf65258823815a4d142facb38e
Mushfiqur Rahman <Mushfiqur Rahman>
2017-09-17 14:58:53 +0600

    Added clear command!

--- index.js
+++ index.js
@@ -36,6 +36,13 @@ const Commands = {
 		} else {
 			msgObj.channel.send(`${author}, Your info format is wrong. Please format it as '/>register <class> <section> <roll>' `)
 		}
+	},
+	"clear": function(parameters, msgObj) {
+		if (message.member.hasPermission("ADMINISTRATOR")) {
+			if (parameters[1] && StringM(parameters[1]).isNumeric && parameters[1] <= 100) {
+				msgObj.channel.bulkDelete(parameters[1])
+			} else {msgObj.channel.bulkDelete();}
+		}
 	}
 }
 
